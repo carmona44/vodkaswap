ddd3bfacbd01340e40d2348c52050997ba8daa13
carmona44 <dcacarmona44@gmail.com>
2022-11-07 19:51:35 +0100

    RESOLVE: exchange contract

--- hardhat/contracts/Exchange.sol
+++ hardhat/contracts/Exchange.sol
@@ -17,4 +17,88 @@ contract Exchange is ERC20 {
     function getReserve() public view returns (uint) {
         return ERC20(jajTokenAddress).balanceOf(address(this));
     }
+
+    function addLiquidity(uint _amount) public payable returns (uint) {
+        uint liquidity;
+        uint ethBalance = address(this).balance;
+        uint jajTokenReserve = getReserve();
+        ERC20 jajToken = ERC20(jajTokenAddress);
+
+        if (jajTokenReserve == 0) {
+            jajToken.transferFrom(msg.sender, address(this), _amount);
+            liquidity = ethBalance;
+            _mint(msg.sender, liquidity);
+        } else {
+            uint ethReserve = ethBalance - msg.value;
+            uint jajTokenAmount = (msg.value * jajTokenReserve) / (ethReserve);
+
+            require(
+                _amount >= jajTokenAmount,
+                "Amount of tokens sent is less than the minimum tokens required"
+            );
+
+            jajToken.transferFrom(msg.sender, address(this), jajTokenAmount);
+            liquidity = (totalSupply() * msg.value) / ethReserve;
+            _mint(msg.sender, liquidity);
+        }
+
+        return liquidity;
+    }
+
+    function removeLiquidity(uint _amount) public returns (uint, uint) {
+        require(_amount > 0, "Amount should be greater than zero");
+        uint ethReserve = address(this).balance;
+        uint _totalSupply = totalSupply();
+        uint ethAmount = (ethReserve * _amount) / _totalSupply;
+        uint jajTokenAmount = (getReserve() * _amount) / _totalSupply;
+
+        _burn(msg.sender, _amount);
+        payable(msg.sender).transfer(ethAmount);
+        ERC20(jajTokenAddress).transfer(msg.sender, jajTokenAmount);
+
+        return (ethAmount, jajTokenAmount);
+    }
+
+    function getAmountOfTokens(
+        uint256 inputAmount,
+        uint256 inputReserve,
+        uint256 outputReserve
+    ) public pure returns (uint256) {
+        require(inputReserve > 0 && outputReserve > 0, "invalid reserves");
+
+        uint256 inputAmountWithFee = inputAmount * 99;
+        uint256 numerator = inputAmountWithFee * outputReserve;
+        uint256 denominator = (inputReserve * 100) + inputAmountWithFee;
+
+        return numerator / denominator;
+    }
+
+    function ethToJajToken(uint _minTokens) public payable {
+        uint256 tokenReserve = getReserve();
+        uint256 tokensBought = getAmountOfTokens(
+            msg.value,
+            address(this).balance - msg.value,
+            tokenReserve
+        );
+
+        require(tokensBought >= _minTokens, "insufficient output amount");
+        ERC20(jajTokenAddress).transfer(msg.sender, tokensBought);
+    }
+
+    function jajTokenToEth(uint _tokensSold, uint _minEth) public {
+        uint256 tokenReserve = getReserve();
+        uint256 ethBought = getAmountOfTokens(
+            _tokensSold,
+            tokenReserve,
+            address(this).balance
+        );
+
+        require(ethBought >= _minEth, "insufficient output amount");
+        ERC20(jajTokenAddress).transferFrom(
+            msg.sender,
+            address(this),
+            _tokensSold
+        );
+        payable(msg.sender).transfer(ethBought);
+    }
 }
